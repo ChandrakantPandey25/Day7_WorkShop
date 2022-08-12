50168d446ca2203fd3b8b935182b87f4b622df31
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 16:00:41 -0700

    Added Larger and smaller number

---
+++ Day7_Workshop/Day7_Workshop/LargerSmaller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7_Workshop
+{
+    class LargerSmaller
+    {
+        public void Largersmaller()
+        {
+            Console.WriteLine("Enter first number");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            if (numOne > numTwo)
+            {
+                Console.WriteLine("Larger number is:" + numOne);
+                Console.WriteLine("Smaller number is:" + numTwo);
+            }
+            else
+            {
+                Console.WriteLine("Larger number is:" + numTwo);
+                Console.WriteLine("Smaller number is:" + numOne);
+            }
+        }
+    }
+}
--- Day7_Workshop/Day7_Workshop/Program.cs
+++ Day7_Workshop/Day7_Workshop/Program.cs
@@ -8,12 +8,18 @@ namespace Day7_Workshop
         {
             Console.WriteLine("Workshop");
             Console.WriteLine("Enter 1 to check entered number is odd or even");
+            Console.WriteLine("Enter 2 to find larger and smaller number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     OddEven resultOne = new OddEven();
                     resultOne.Oddeven();
+                    break;
+                case 2:
+                    LargerSmaller resultTwo = new LargerSmaller();
+                    resultTwo.Largersmaller();
+
                     break;
                 default:
                     Console.WriteLine("Enter valid number");
