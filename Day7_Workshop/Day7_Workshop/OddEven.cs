4ca114b4fcb92e77d905633ee906e8381458b329
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 15:56:51 -0700

    Added Oddeven

---
+++ Day7_Workshop/Day7_Workshop/OddEven.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7_Workshop
+{
+    class OddEven
+    {
+        public void Oddeven()
+        {
+            Console.WriteLine("Enter first number");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            if (numOne % 2 == 0)
+            {
+                Console.WriteLine("Entered number num1 is even");
+
+            }
+            else
+            {
+                Console.WriteLine("Entered number num1 is odd");
+            }
+            if (numTwo % 2 == 0)
+            {
+                Console.WriteLine("Entered number num2 is even");
+            }
+            else
+            {
+                Console.WriteLine("Entered number num2 is odd");
+            }
+        }
+    }
+}
---
+++ Day7_Workshop/Day7_Workshop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day7_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Workshop");
+            Console.WriteLine("Enter 1 to check entered number is odd or even");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    OddEven resultOne = new OddEven();
+                    resultOne.Oddeven();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid number");
+                    break;
+            }
+        }
+    }
+}
